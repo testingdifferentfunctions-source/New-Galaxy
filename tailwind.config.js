2fc31c47bd8619c6040b4f9356cf53848c647db5
magnifiquedigitalworld <testingdifferentfunctions@gmail.com>
2026-08-06 13:10:20 +0300

    Refactor tailwind.config.js to use ES module syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import tailwindcssAnimate from "tailwindcss-animate";
+
+export default {
     darkMode: ["class"],
     content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
   theme: {
@@ -91,5 +93,6 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 }
+
